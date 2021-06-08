2af18206054575e8c6c645a03d0c2e36cf1213ce
Nilay <nilay.pandya3@gmail.com>
2021-06-08 19:04:33 +0530

    first commit

--- app/src/main/java/com/example/internshipfinal/MainActivity.java
+++ app/src/main/java/com/example/internshipfinal/MainActivity.java
@@ -72,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Facebook Starts
+                //Facebook Starts
     register = findViewById(R.id.textView2);
     cardView = findViewById(R.id.cardView);
     user = findViewById(R.id.editText);
@@ -112,154 +112,137 @@ public class MainActivity extends AppCompatActivity {
 
         //Google Starts
 
-        //Initializing Views
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestEmail()
+                        .build();
+
+                mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+                signInButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        signIn();
+                    }
+                });
 
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestEmail()
-                .build();
 
-        // Build a GoogleSignInClient with the options specified by gso.
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+    }
 
-        signInButton.setOnClickListener(new View.OnClickListener() {
+
+                     //Google Starts
+
+            private void signIn() {
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+            }
+
             @Override
-            public void onClick(View view) {
-                signIn();
+            public void onActivityResult(int requestCode, int resultCode, Intent data) {
+                callbackManager.onActivityResult(requestCode, resultCode, data);
+                super.onActivityResult(requestCode, resultCode, data);
+                if (requestCode == RC_SIGN_IN) {
+                    Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+                    handleSignInResult(task);
+                }
             }
-        });
-
-
-        //Google Ends
-    }
-
-
-    //Google Starts
-
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        callbackManager.onActivityResult(requestCode, resultCode, data);
-        super.onActivityResult(requestCode, resultCode, data);
-//Google Starts
-
-        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            // The Task returned from this call is always completed, no need to attach
-            // a listener.
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-            handleSignInResult(task);
-        }
-        //Google Ends
-    }
 
 
 
 
-    private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
-        try {
-            GoogleSignInAccount account = completedTask.getResult(ApiException.class);
-            // Signed in successfully, show authenticated UI.
-            startActivity(new Intent(MainActivity.this, googlesignedin.class));
-        } catch (ApiException e) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("Google Sign In Error", "signInResult:failed code=" + e.getStatusCode());
-            Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_LONG).show();
-        }
-    }
+            private void handleSignInResult(Task<GoogleSignInAccount> completedTask) {
+                try {
+                    GoogleSignInAccount account = completedTask.getResult(ApiException.class);
+                    startActivity(new Intent(MainActivity.this, googlesignedin.class));
+                } catch (ApiException e) {
+                    Log.w("Google Sign In Error", "signInResult:failed code=" + e.getStatusCode());
+                    Toast.makeText(MainActivity.this, "Failed", Toast.LENGTH_LONG).show();
+                }
+            }
 
-    @Override
-    protected void onStart() {
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
-        if(account != null) {
-            startActivity(new Intent(MainActivity.this, googlesignedin.class));
-        }
-        super.onStart();
-    }
-    //Google Ends
+            @Override
+            protected void onStart() {
+                GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+                if(account != null) {
+                    startActivity(new Intent(MainActivity.this, googlesignedin.class));
+                }
+                super.onStart();
+            }
+                     //Google Ends
 
 
-    //Facebook Starts
+                //Facebook Starts
 
 
-    AccessTokenTracker tokenTracker = new AccessTokenTracker() {
-        @Override
-        protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken)
-        {
-            if(currentAccessToken==null)
+        AccessTokenTracker tokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken, AccessToken currentAccessToken)
             {
-                txtname.setText("");
-                txtemail.setText("");
+                if(currentAccessToken==null)
+                {
+                    txtname.setText("");
+                    txtemail.setText("");
 
-                circleImageView.setImageResource(0);
-                Toast.makeText(MainActivity.this,"User Logged Out",Toast.LENGTH_SHORT).show();
-                Intent intent = new Intent(MainActivity.this,MainActivity.class);
-                startActivity(intent);
+                    circleImageView.setImageResource(0);
+                    Toast.makeText(MainActivity.this,"User Logged Out",Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(MainActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    loaduserprofile(currentAccessToken);
+                }
             }
-            else{
-                loaduserprofile(currentAccessToken);
-            }
-        }
     };
 
     private void loaduserprofile(AccessToken newaccessToken)
-    {
-        GraphRequest request = GraphRequest.newMeRequest(newaccessToken, new GraphRequest.GraphJSONObjectCallback() {
-            @Override
-            public void onCompleted(JSONObject object, GraphResponse response)
-            {
-                try {
+        {
+           GraphRequest request = GraphRequest.newMeRequest(newaccessToken, new GraphRequest.GraphJSONObjectCallback() {
+                    @Override
+                    public void onCompleted(JSONObject object, GraphResponse response)
+                    {
+                        try {
 
-                    String first_name = object.getString("first_name");
-                    String last_name = object.getString("last_name");
-                    String email = object.getString("email");
-                    String id = object.getString("id");
+                            String first_name = object.getString("first_name");
+                            String last_name = object.getString("last_name");
+                            String email = object.getString("email");
+                            String id = object.getString("id");
 
-                    String image_url = "https://graph.facebook.com/" + id + "/picture?type=normal";
-                    txtname.setText(first_name + " " + last_name);
-                    txtemail.setText(email);
-                    RequestOptions requestOptions = new RequestOptions();
-                    requestOptions.dontAnimate();
-                    txtname.setVisibility(View.VISIBLE);
-                    txtemail.setVisibility(View.VISIBLE);
-                    signInButton.setVisibility(View.GONE);
-                    register.setVisibility(View.GONE);
-                    cardView.setVisibility(View.GONE);
-                    user.setVisibility(View.GONE);
-                    pass.setVisibility(View.GONE);
-                    Glide.with(MainActivity.this).load(image_url).into(circleImageView);
+                            String image_url = "https://graph.facebook.com/" + id + "/picture?type=normal";
+                            txtname.setText(first_name + " " + last_name);
+                            txtemail.setText(email);
+                            RequestOptions requestOptions = new RequestOptions();
+                            requestOptions.dontAnimate();
+                            txtname.setVisibility(View.VISIBLE);
+                            txtemail.setVisibility(View.VISIBLE);
+                            signInButton.setVisibility(View.GONE);
+                            register.setVisibility(View.GONE);
+                            cardView.setVisibility(View.GONE);
+                            user.setVisibility(View.GONE);
+                            pass.setVisibility(View.GONE);
+                            Glide.with(MainActivity.this).load(image_url).into(circleImageView);
 
-                }
-                catch (JSONException e)
-                {
-                    e.printStackTrace();
+                        }
+                        catch (JSONException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields","first_name,last_name,email,id");
+                request.setParameters(parameters);
+                request.executeAsync();
+
+
+    }
+
+                private void checkloginstatus(){
+                    if(AccessToken.getCurrentAccessToken()!= null){
+                        loaduserprofile(AccessToken.getCurrentAccessToken());
                 }
             }
-        });
-        Bundle parameters = new Bundle();
-        parameters.putString("fields","first_name,last_name,email,id");
-        request.setParameters(parameters);
-        request.executeAsync();
-
-
-    }
-
-    private void checkloginstatus(){
-        if(AccessToken.getCurrentAccessToken()!= null){
-            loaduserprofile(AccessToken.getCurrentAccessToken());
-        }
-    }
 
 
 
-    //Facebook Ends
+                 //Facebook Ends
 }
--- app/src/main/java/com/example/internshipfinal/googlesignedin.java
+++ app/src/main/java/com/example/internshipfinal/googlesignedin.java
@@ -44,13 +44,10 @@ public class googlesignedin extends AppCompatActivity {
         idTV = findViewById(R.id.id);
         photoIV = findViewById(R.id.photo);
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
                 .requestEmail()
                 .build();
 
-        // Build a GoogleSignInClient with the options specified by gso.
         mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(googlesignedin.this);
